b6575aff5ea321171071b271613031b90f67535f
MFORCE\takuya_nakai <takuya_nakai@mforce.co.jp>
2024-10-02 12:53:40 +0900

    add classpath for lib dir

--- lambda_function.py
+++ lambda_function.py
@@ -1,3 +1,8 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from lib.services.producer_service import ProducerService
 
 def lambda_hander(event, context):
